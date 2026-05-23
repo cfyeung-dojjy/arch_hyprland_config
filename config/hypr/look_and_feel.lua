f765d2f8fb722b977eb72e61ef23c7ac53895083
John Yeung <cfyeung2357@gmail.com>
2026-05-22 18:50:28 -0700

    re-set border_size=1 to better indicate focused window

--- config/hypr/look_and_feel.lua
+++ config/hypr/look_and_feel.lua
@@ -4,7 +4,7 @@ hl.config({
         gaps_in          = 5,
         gaps_out         = 0,
 
-        border_size      = 0,
+        border_size      = 1,
 
         col              = {
             active_border   = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
