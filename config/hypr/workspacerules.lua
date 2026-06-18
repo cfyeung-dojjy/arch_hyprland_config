3962ac7f790794b632f100ccd86083d48be78e08
dojjy <cfyeung2357@gmail.com>
2026-06-17 22:03:19 -0700

    laptop computer only has one monitor by default

--- config/hypr/workspacerules.lua
+++ config/hypr/workspacerules.lua
@@ -1,9 +1,5 @@
 require("programs")
 
 for i = 1, 10 do
-    hl.workspace_rule({ workspace = i, monitor = main_monitor, default = (i == 1) })
-end
-
-for i = 11, 20 do
-    hl.workspace_rule({ workspace = i, monitor = left_monitor, default = (i == 11) })
+    hl.workspace_rule({ workspace = i, default = (i == 1) })
 end
